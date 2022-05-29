87433c1936acd3e3b3cae2a34ae1902cb8a6e8f7
“nmthangdn20000” <uptimumdn2000@gmail.com>
2022-05-29 01:29:56 -0700

    .

--- socket/index.socket.js
+++ socket/index.socket.js
@@ -2,21 +2,36 @@ import { Server } from 'socket.io';
 import { verifyUserSocket } from '../middlewares/authentic.middleware';
 import likeSocket from './like.socket';
 
+let io = null;
+let socket = null;
+
 const socketIo = (server) => {
-  const io = new Server(server, {
+  io = new Server(server, {
     cors: { origin: getCorsOrigins(), credentials: true },
   });
 
   io.use(verifyUserSocket);
 
-  const onConnection = (socket) => {
-    likeSocket(io, socket);
+  const onConnection = (s) => {
+    socket = s;
+
+    console.log('onConnection ', socket.id);
+    // likeSocket(io, s);
+    socket.on('disconnecting', () => {
+      console.log(socket.rooms);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnect ', socket.id);
+    });
   };
 
   io.on('connection', onConnection);
 };
 
 export default socketIo;
+
+export { io, socket };
 
 function getCorsOrigins() {
   const origins = process.env.CORS_ORIGINS;
