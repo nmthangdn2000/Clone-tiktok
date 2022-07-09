3687d396ddd7bc33a23940c582295a2d55bd4c49
“nmthangdn20000” <uptimumdn2000@gmail.com>
2022-07-09 21:22:05 +0700

    add slider process bar video

--- src/screens/home/components/VideoItem.js
+++ src/screens/home/components/VideoItem.js
@@ -3,20 +3,28 @@ import React, { useImperativeHandle, useRef, useState } from 'react';
 import VerticalSecction from './VerticalSecction';
 import BottomSecction from './BottomSecction';
 import Video from 'react-native-video';
-import { Container } from '../../../components';
+import { Container, CText } from '../../../components';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import { HEIGHT } from '../../../configs/constant';
+import { HEIGHT, WIDTH } from '../../../configs/constant';
 import PressContainer from './PressContainer';
 import { SERVER_DOMAIN } from '../../../constants/constants';
+import Slider from '@react-native-community/slider';
+import { COLOR, SPACING } from '../../../configs/styles';
 
 const { width, height } = Dimensions.get('window');
 
 const VideoItem = React.forwardRef(({ item }, ref) => {
   const { _id, caption, url, author, audio, like, comment } = item;
   const verticalRef = useRef();
+  const videoRef = useRef();
+  const sliderRef = useRef();
+
   const bottomHeight = useBottomTabBarHeight();
 
   const [isActive, setIsActive] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [seek, setSeek] = useState(0);
 
   useImperativeHandle(ref, () => ({
     pauseVideo: () => {
@@ -41,6 +49,7 @@ const VideoItem = React.forwardRef(({ item }, ref) => {
       height={HEIGHT - bottomHeight - StatusBar.currentHeight}
       backgroundColor="black">
       <Video
+        ref={videoRef}
         source={{
           uri: `${SERVER_DOMAIN}/${url}`,
         }}
@@ -48,6 +57,11 @@ const VideoItem = React.forwardRef(({ item }, ref) => {
         resizeMode="cover"
         paused={!isActive}
         repeat
+        onLoad={data => setDuration(data.duration)}
+        onProgress={data => {
+          setSliderValue(data.currentTime / duration);
+        }}
+        seek={seek}
       />
 
       <PressContainer
@@ -73,6 +87,31 @@ const VideoItem = React.forwardRef(({ item }, ref) => {
         author={author}
         idVideo={_id}
       />
+
+      <Container
+        width={WIDTH}
+        padding={0}
+        position="absolute"
+        bottom={-SPACING.S1 - 4}
+        elevation={100}>
+        <Slider
+          ref={sliderRef}
+          style={{
+            left: -SPACING.S2 * 2 + SPACING.S1,
+            width: WIDTH + SPACING.S3 * 2,
+          }}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor={COLOR.WHITE}
+          maximumTrackTintColor={COLOR.WHITE}
+          thumbTintColor={COLOR.WHITE}
+          value={sliderValue}
+          onSlidingComplete={value => {
+            setSeek(value * duration);
+            setSliderValue(value);
+          }}
+        />
+      </Container>
     </Container>
   );
 });
